e1e85bb609dd9d64fd380701a2236c4f02bfc8c8
chris-cushing <chris.cushing@outlook.com>
2025-06-13 21:24:46 -0700

    fix nvim-cmp to not auto select auto complete
    This is probably in the wrong place and done the wrong way, but needed
    to make this work in a less stupid way

--- private_dot_config/nvim/lua/options.lua
+++ private_dot_config/nvim/lua/options.lua
@@ -1,6 +1,26 @@
 require "nvchad.options"
 
 -- add yours here!
+local cmp = require "cmp"
+
+cmp.setup({
+  mapping = {
+     ["<CR>"] = cmp.mapping({
+       i = function(fallback)
+         if cmp.visible() and cmp.get_active_entry() then
+           cmp.confirm({ behavior = cmp.ConfirmBehavior.Replace, select = false })
+         else
+           fallback()
+         end
+       end,
+       s = cmp.mapping.confirm({ select = true }),
+       c = cmp.mapping.confirm({ behavior = cmp.ConfirmBehavior.Replace, select = true }),
+     }),
+     ...
+  }
+})
+
+return cmp
 
 -- local o = vim.o
 -- o.cursorlineopt ='both' -- to enable cursorline!
