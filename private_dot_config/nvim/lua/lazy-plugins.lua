f30feae23007a3e445a58c3ddbddf637ec487462
chris-cushing <chris.cushing@outlook.com>
2026-01-10 14:32:40 -0800

    turn on debugger
    has the full ui, should try out the minimal version instead

--- private_dot_config/nvim/lua/lazy-plugins.lua
+++ private_dot_config/nvim/lua/lazy-plugins.lua
@@ -54,7 +54,7 @@ require("lazy").setup({
 	--  Here are some example plugins that I've included in the Kickstart repository.
 	--  Uncomment any of the lines below to enable them (you will need to restart nvim).
 	--
-	-- require 'kickstart.plugins.debug',
+	require("plugins.debug"),
 	require("plugins.indent_line"),
 	-- require 'kickstart.plugins.lint',
 	-- require 'kickstart.plugins.autopairs',
